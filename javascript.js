e7e30de4c5487f69cb20d03da8a71611e59b6175
peterotim <petercharlesotim@gmail.com>
2022-05-03 11:17:10 +0300

    initial commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("humburgerBtn").classList.toggle("open");
+
+}const x = document.getElementById("humburgerBtn")
+x.onclick = toggleMenu
